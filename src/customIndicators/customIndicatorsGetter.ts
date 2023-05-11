1ade948903981c412111c7ad392c8b1513355331
lmvdz <lmvanderzande@gmail.com>
2023-05-10 22:11:55 -0500

    EMA Ribbon

--- src/customIndicators/customIndicatorsGetter.ts
+++ src/customIndicators/customIndicatorsGetter.ts
@@ -2,6 +2,7 @@ import { PVSRA_Combined } from "./PVSRA_Combined";
 import { PVSRA_Candles } from "./PVSRA_Candles";
 import { PVSRA_Histogram } from "./PVSRA_Histogram";
 import { SmoothedVWAP } from "./VWAP";
+import { EMA_Ribbon } from "./EMARibbon";
 
 export function customIndicatorsGetter(PineJS: any) {
   return Promise.resolve([
@@ -9,6 +10,7 @@ export function customIndicatorsGetter(PineJS: any) {
     PVSRA_Candles(PineJS),
     PVSRA_Histogram(PineJS),
     SmoothedVWAP(PineJS),
+    EMA_Ribbon(PineJS),
     //...
   ]);
 }
